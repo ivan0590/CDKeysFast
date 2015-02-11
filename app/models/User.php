e22d251f96927ce724a112737c75edc8b844b5ea
Iván Quintas <ivan0590@gmail.com>
2015-02-11 19:41:53 +0100

    Cambio en el modelo User

--- app/models/User.php
+++ app/models/User.php
@@ -21,7 +21,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password', 'remember_token');
+	protected $hidden = array('confirmation_code', 'password', 'remember_token');
         
         public function userable () {
             return $this->morphTo();
