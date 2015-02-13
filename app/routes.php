f4fdc138a3fec2fc4e49ada97d4b60ff894e7782
Iván Quintas <ivan0590@gmail.com>
2015-02-13 18:59:27 +0100

    Cambios en las rutas
    - Se ha cambiado la ruta de la página de inicio porque daba problemas
    con la paginación en las páginas. Ahora para ordenar los productos se
    utiliza un filtro que se encarga de validar los parámetros de
    ordenación.
    - Se ha añadido una ruta para las búsquedas.

--- app/routes.php
+++ app/routes.php
@@ -11,21 +11,29 @@
   |
  */
 
-
-Route::get('/{sort?}', [
+//Raíz
+Route::get('/', [
     'as' => 'index',
-    'uses' => 'StoreFrontController@getIndex'
+    'uses' => 'StoreFrontController@getIndex',
+    'before' => 'sort'
 ]);
 
+//Login y logout
 Route::resource('session', 'SessionController', ['only' => ['store', 'destroy']]);
 
+//Búsquedas
+Route::resource('search', 'SearchController', ['only' => ['create', 'show']]);
+
+//Usuarios
 Route::resource('user', 'UserController', ['except' => ['index', 'update']]);
 
+//Confirmar email de usuario
 Route::get('user/create/confirm/{confirmationCode}', [
     'as' => 'confirmation_path',
     'uses' => 'UserController@confirm'
 ]);
 
+//Mostrar mensaje
 Route::get('/info', [
     'as' => 'info',
     'uses' => 'StoreFrontController@getInfo'
